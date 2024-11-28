5d645c862dcfb3072ba3960554107c492cbaec0c
yegeto <115154804+yegeto@users.noreply.github.com>
2024-11-28 11:12:16 +0300

    write the logic to play the entire game

--- script.js
+++ script.js
@@ -9,25 +9,38 @@ const getComputerChoice = () => {
 
 const getHumanChoice = () => prompt("Rock? Paper? Scissors?");
 
-let humanScore = 0;
-let computerScore = 0;
+const playGame = () => {
+  let humanScore = 0;
+  let computerScore = 0;
 
-const playRound = (humanChoice, computerChoice) => {
-  humanChoice =
-    humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-  if (humanChoice === computerChoice) {
-    return "It's draw";
-  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-    humanScore++;
-    return "You win! Rock beats Scissors.";
-  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-    humanScore++;
-    return "You win! Paper beats Rock.";
-  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-    humanScore++;
-    return "You win! Scissors beats Paper.";
-  } else {
-    computerScore++;
-    return `You lose! ${computerChoice} beats ${humanChoice}.`;
+  const playRound = (humanChoice, computerChoice) => {
+    humanChoice =
+      humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    if (humanChoice === computerChoice) {
+      return "It's draw";
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+      humanScore++;
+      return "You win! Rock beats Scissors.";
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+      humanScore++;
+      return "You win! Paper beats Rock.";
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+      humanScore++;
+      return "You win! Scissors beats Paper.";
+    } else {
+      computerScore++;
+      return `You lose! ${computerChoice} beats ${humanChoice}.`;
+    }
+  };
+
+  let roundCounter = 0;
+
+  while (roundCounter < 5) {
+    const roundResult = playRound(getHumanChoice(), getComputerChoice());
+    const currentScore = `\nYour Score: ${humanScore}\nComputer Score: ${computerScore}`;
+    console.log(roundResult, currentScore);
+    roundCounter++;
   }
 };
+
+playGame();
