b24f038a8ec6eb48915b67c6cc57d7f72ec96102
yegeto <115154804+yegeto@users.noreply.github.com>
2024-11-28 10:38:56 +0300

    write the logic to play a single round

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ const getHumanChoice = () => prompt("Rock? Paper? Scissors?");
 
 let humanScore = 0;
 let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+  humanChoice =
+    humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+  if (humanChoice === computerChoice) {
+    return "It's draw";
+  } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    humanScore++;
+    return "You win! Rock beats Scissors.";
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    humanScore++;
+    return "You win! Paper beats Rock.";
+  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    humanScore++;
+    return "You win! Scissors beats Paper.";
+  } else {
+    computerScore++;
+    return `You lose! ${computerChoice} beats ${humanChoice}.`;
+  }
+};
