920fafefbb6f97e89e16c83f93f58ce653b609d8
yegeto <115154804+yegeto@users.noreply.github.com>
2024-11-28 09:34:08 +0300

    write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const getComputerChoice = () => {
+  const getRandomNumber = Math.floor(Math.random() * 3);
+  return getRandomNumber === 0
+    ? "Rock"
+    : getRandomNumber === 1
+    ? "Paper"
+    : "Scissors";
+};
